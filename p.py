f23434792440703f15b0d6a6901f936d3a024b6c
ABC <abc@admins-Mac-mini.local>
2017-06-27 02:38:41 -0700

    first commit

---
+++ p.py
@@ -0,0 +1,16 @@
+
+def add():
+    a = 10;
+    b = 20;
+    y = a + b
+    return y
+def subtract():
+    a = 10;
+    b = 20;
+    z = a - b
+    return z
+
+print "maths="
+age=add()
+height = subtract()
+print "age and height is", age,height
